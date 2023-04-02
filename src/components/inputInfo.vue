a9d71b5b32a940df6122a3b4650e6f1ab326dfbb
LathanChen <czm316@gmail.com>
2023-04-02 21:10:07 +0900

    时间：4月2日 增加了修改和删除信息的后台操作

--- src/components/inputInfo.vue
+++ src/components/inputInfo.vue
@@ -2,7 +2,12 @@
   <div class="indexPage">
     <div class="chaxun" :style="{ width: cxdivwidth + '%' }">
       <!-- model-value指定最初默认打开的标签页 -->
-      <el-tabs model-value="first" v-model="activeName" @tab-click="tabClick">
+      <el-tabs
+        model-value="first"
+        v-model="activeName"
+        @tab-click="tabClick"
+        lazy="true"
+      >
         <el-tab-pane label="新增信息" name="first">
           <el-form :model="form" label-width="80px">
             <el-form-item label="场馆名">
@@ -111,6 +116,7 @@
               <el-col :span="8">
                 <el-form-item>
                   <el-popconfirm @confirm="insertInfo" title="确认要添加吗">
+                    <!-- <template #reference> 会创建一个匿名插槽，其中的内容将被插入到组件的默认插槽中,也就是把下面的el-button放入到el-popconfirm的默认插槽中 -->
                     <template #reference>
                       <el-button type="primary">添加</el-button>
                     </template>
@@ -150,7 +156,9 @@
               </el-select>
             </el-form-item>
           </el-form>
-          <el-button type="primary" @click="showall" v-if="cxButtonShow">查询</el-button>
+          <el-button type="primary" @click="showall" v-if="cxButtonShow"
+            >查询</el-button
+          >
           <!-- 设置div进入时的动画效果，appear代表初次渲染时依然会出现动画 -->
           <!-- enter-active-class进入动画
           leave-active-class离开动画 -->
@@ -160,20 +168,96 @@
             enter-active-class="animate__backInUp"
           >
             <div id="editMingxi" v-if="editMingxi">
-              <el-table :data="editFormData.array" style="width: 100%">
-    <el-table-column prop="zhoushu" width="120" />
-    <el-table-column prop="zhouji" width="120" />
-    <el-table-column prop="ks_shijian"  label="开始时间" width="120"/>
-    <el-table-column prop="js_shijian" label="结束时间" width="120"/>
-    <el-table-column fixed="right" label="操作选项">
-      <template #default>
-        <el-button link type="primary" size="small"
-          >编辑</el-button
-        >
-        <el-button link type="primary" size="small">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+              <el-table
+                :data="editFormData.array"
+                style="width: 100%"
+                edit-config="true"
+              >
+                <!-- prop：用于指定数据对象中与表格列相对应的属性名,即绑定的数据对象editFormData.array的属性值 -->
+                <el-table-column width="120">
+                  <!-- #default="{ row }，el-table-column将数据显示在默认插槽，row为表格绑定的editFormData.array数组的第一个对象 -->
+                  <template v-slot:default="{ row }">
+                    <!-- 在row元素中增加edit属性，控制每行的可编辑状态 -->
+                    <el-select v-model="row.zhoushu" :disabled="!row.edit">
+                      <!-- 这里一定要使用:value="1"的方式，实现动态数据绑定，不然后台的row.zhoushu不会绑定到每一个选项中 -->
+                      <el-option label="第一周" :value="1" />
+                      <el-option label="第二周" :value="2" />
+                      <el-option label="第三周" :value="3" />
+                      <el-option label="第四周" :value="4" />
+                      <el-option label="第五周" :value="5" />
+                      <el-option label="每周" :value="6" />
+                    </el-select>
+                    <!-- <el-input v-model="row.zhoushu_zw" /> -->
+                  </template>
+                </el-table-column>
+                <el-table-column width="120">
+                  <!-- #default="{ row }，el-table-column将数据显示在默认插槽，row为表格绑定的editFormData.array数组的第一个对象 -->
+                  <template v-slot:default="{ row }">
+                    <el-select v-model="row.zhouji" placeholder="请选择星期数" :disabled="!row.edit">
+                      <el-option label="周一" :value="1" />
+                      <el-option label="周二" :value="2" />
+                      <el-option label="周三" :value="3" />
+                      <el-option label="周四" :value="4" />
+                      <el-option label="周五" :value="5" />
+                      <el-option label="周六" :value="6" />
+                      <el-option label="周日" :value="0" />
+                    </el-select>
+                  </template>
+                </el-table-column>
+                <el-table-column label="开始时间" width="200">
+                  <template v-slot:default="{ row }">
+                    <!-- <el-time-picker
+                      v-model="row.ks_shijian"
+                      size="small"
+                      is-range
+                      range-separator="To"
+                      start-placeholder="Start time"
+                      end-placeholder="End time"
+                    /> -->                   
+              <el-time-select
+                v-model="row.ks_shijian"
+                start="08:30"
+                step="00:30"
+                end="22:00"
+                :disabled="!row.edit"
+              />
+                  </template>
+                </el-table-column>
+                <el-table-column label="结束时间" width="200">
+                  <template #default="{ row }">
+              <el-time-select
+                v-model="row.js_shijian"
+                start="08:30"
+                step="00:30"
+                end="22:00"
+                :disabled="!row.edit"
+              />
+                  </template>
+                </el-table-column>
+                <!-- <el-table-column
+                  prop="js_shijian"
+                  label="结束时间"
+                  width="120"
+                /> -->
+                <el-table-column fixed="right" label="操作选项">
+                  <!-- 可以写成下面这样，$index代表row对象在列表中的索引值
+                  <template v-slot:default="{ row,$index }"> -->
+                  <template v-slot:default="{ row }">
+                    <!-- 通过按钮的click方法，为edit属性赋值，控制按钮的显示或隐藏，即同一行其他单元格的可编辑状态 -->
+                    <el-button link type="primary" size="small" @click="bianjimingxi(row)" v-if="!row.edit">编辑</el-button>
+                    <el-popconfirm @confirm="baocunmingxi(row)" title="确认要保存吗" v-if="row.edit">
+                    <template #reference>
+                    <el-button link type="primary" size="small">保存</el-button>
+                    </template>
+                    </el-popconfirm>
+                    <el-popconfirm @confirm="deletemingxi(row)" title="确认要删除吗">
+                    <template #reference>
+                    <el-button link type="primary" size="small">删除</el-button>
+                    </template>
+                    </el-popconfirm>
+                  </template>
+                </el-table-column>
+              </el-table>
             </div>
           </transition>
         </el-tab-pane>
@@ -234,9 +318,10 @@ export default {
       zhouji: "",
     });
 
+    // 这里必须写成对象形式，不然v-modle读不到数据
     let editFormData = reactive({
-      array:[]
-    })
+      array: [],
+    });
 
     const editForm = reactive({
       cdbianhao: "",
@@ -270,6 +355,9 @@ export default {
     let js_shijian = ref(true);
     let cxdivwidth = ref(20);
     let editMingxi = ref(false);
+    let editMingxi_ksshijian = ref("");
+    let editMingxidisplay = ref(true);
+
 
     // 监视form的shijianduan属性，这里watch方法的第一个参数必须写成一个函数，并且要开启深度监视
     // 原因：在Vue 3中，watch函数的第一个参数可以是一个函数，也可以是一个响应式对象或一个ref。如果第一个参数是一个响应式对象或一个ref，Vue 3会自动将其转换成一个getter函数，然后使用该函数来获取属性的值进行监视。因此，如果我们需要监视一个响应式对象或一个ref的属性，可以直接将其作为第一个参数传递给watch函数。
@@ -370,6 +458,8 @@ export default {
     //   deep:true
     // }
     // )
+    
+
 
     function insertInfo() {
       // toRaw() 是 Vue.js 3 中的一个函数。它是一个全局函数，可用于将响应式对象转换为普通对象
@@ -392,77 +482,104 @@ export default {
     function tabClick(tab) {
       // 获取点击标签的name要使用tab.props.name
       // console.log(tab.props.name)
-      if (tab.props.name == 'first'){
-      cxdivwidth.value = 20;
-      editFormShow.value = true;
-      editMingxi.value = false;
-      cxButtonShow.value = true
+      if (tab.props.name == "first") {
+        cxdivwidth.value = 20;
+        editFormShow.value = true;
+        editMingxi.value = false;
+        cxButtonShow.value = true;
       }
-      else {
-        return
-      }
+
       // instance.proxy.$router.push({ path: "/indexpage" });
     }
 
     // function show(){
     //   mingxi.value = !mingxi.value
     // }
+    function showallEditArray(){
+      axios.post("/api/editanddeleteinfo", toRaw(editForm)).then((response) => {
+        // 使用foreach对数组进行循环
+        editFormData.array = response.data;
+        editFormData.array.forEach((elementit) => {
+          switch (elementit.zhouji) {
+            case 1:
+              elementit.zhouji_zw = "周一";
+              break;
+            case 2:
+              elementit.zhouji_zw = "周二";
+              break;
+            case 3:
+              elementit.zhouji_zw = "周三";
+              break;
+            case 4:
+              elementit.zhouji_zw = "周四";
+              break;
+            case 5:
+              elementit.zhouji_zw = "周五";
+              break;
+            case 6:
+              elementit.zhouji_zw = "周六";
+              break;
+            case 0:
+              elementit.zhouji_zw = "周日";
+              break;
+          }
+          switch (elementit.zhoushu) {
+            case 1:
+              elementit.zhoushu_zw = "第一周";
+              break;
+            case 2:
+              elementit.zhoushu_zw = "第二周";
+              break;
+            case 3:
+              elementit.zhoushu_zw = "第三周";
+              break;
+            case 4:
+              elementit.zhoushu_zw = "第四周";
+              break;
+            case 5:
+              elementit.zhoushu_zw = "第五周";
+              break;
+            case 6:
+              elementit.zhoushu_zw = "每周";
+              break;
+          }
+        });
+        console.log(editFormData.array);
+      });
 
+    }
     function showall() {
       cxdivwidth.value = 50;
       editFormShow.value = !editFormShow.value;
       editMingxi.value = !editMingxi.value;
-      cxButtonShow.value = !cxButtonShow.value
-      axios.post("/api/editanddeleteinfo", toRaw(editForm)).then((response) => {
-        response.data.forEach(elementit => {
-          switch(elementit.zhouji){
-            case 1:
-            elementit.zhouji = '周一'
-            break
-            case 2:
-            elementit.zhouji = '周二'
-            break
-            case 3:
-            elementit.zhouji = '周三'
-            break
-            case 4:
-            elementit.zhouji = '周四'
-            break
-            case 5:
-            elementit.zhouji = '周五'
-            break
-            case 6:
-            elementit.zhouji = '周六'
-            break
-            case 0:
-            elementit.zhouji = '周日'
-            break
-          }
-          switch(elementit.zhoushu){
-            case 1:
-            elementit.zhoushu = '第一周'
-            break
-            case 2:
-            elementit.zhoushu = '第二周'
-            break
-            case 3:
-            elementit.zhoushu = '第三周'
-            break
-            case 4:
-            elementit.zhoushu = '第四周'
-            break
-            case 5:
-            elementit.zhoushu = '第五周'
-            break
-            case 6:
-            elementit.zhoushu = '每周'
-            break
-          }
-        });       
-          editFormData.array = response.data
-          console.log(editFormData.array)
-      });
+      cxButtonShow.value = !cxButtonShow.value;
+      showallEditArray()
     }
+
+    function bianjimingxi(row){
+        row.edit = true
+        // showbaocun.value = !showbaocun.value
+        // bianji.value = !bianji.value    
+      }
+
+    function baocunmingxi(row){
+      row.edit = false
+        // showbaocun.value = !showbaocun.value
+        // bianji.value = !bianji.value 
+        axios.post("/api/editinfo", toRaw(row))
+      }
+
+    function deletemingxi(row){
+      row.edit = false
+        // showbaocun.value = !showbaocun.value
+        // bianji.value = !bianji.value 
+        axios.delete(`/api/deleteinfo/${row.bianhao}`).then((rensponse) =>{
+          if(rensponse.data == true){
+            console.log(rensponse.data)
+            showallEditArray()
+          }
+        })
+      }
 
     return {
       form,
@@ -471,6 +588,9 @@ export default {
       insertInfo,
       showall,
       tabClick,
+      bianjimingxi,
+      baocunmingxi,
+      deletemingxi,
       listvalues,
       ks_shijian_from,
       js_shijian_from,
@@ -486,7 +606,11 @@ export default {
       editMingxi,
       // show,
       editFormShow,
-      cxButtonShow
+      cxButtonShow,
+      editMingxi_ksshijian,
+      editMingxidisplay,
+      // bianji,
+      // showbaocun
     };
   },
 };
